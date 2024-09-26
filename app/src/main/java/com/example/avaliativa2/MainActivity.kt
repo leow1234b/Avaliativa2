08ed4e3021d7d866c58400f8b3176eb3685b3d5d
Auto Logon <autologon@CSED.LAB>
2024-09-26 10:33:42 -0300

    avaliativa2

--- app/src/main/java/com/example/avaliativa2/MainActivity.kt
+++ app/src/main/java/com/example/avaliativa2/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.avaliativa2
 
-import androidx.compose.ui.Modifier
 
-
-
-import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 
 data class Player(
@@ -31,7 +37,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PlayerProgressApp() {
-    // Usando um estado para gerenciar a lista de jogadores
     val players = remember { mutableStateListOf<Player>().apply { repeat(6) { add(Player()) } } }
 
     Column(modifier = Modifier.padding(16.dp)) {
